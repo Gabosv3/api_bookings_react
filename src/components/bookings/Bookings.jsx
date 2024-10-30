df073240d4be27a5a53a9beed061918b78fa3423
Josías <josiasalvarengaromero@gmail.com>
2024-10-30 00:42:15 -0600

    Adding logic to booking create form
    first approach

--- src/components/bookings/Bookings.jsx
+++ src/components/bookings/Bookings.jsx
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
+import NewBookingModal from "./NewBookingModal";
 
- const Bookings = () => {
-  return (
-    <div>
-      <h1>Reservaciones</h1>
-      <p>Aquí está el contenido de Reservaciones.</p>
-    </div>
-  );
-};
+const Bookings = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    return (
+        <div>
+            <div className="row align-items-center justify-content-center">
+                <div className="col-md-5 col-sm-12">
+                    <h1 className="mb-3">Lista de Reservaciones</h1>
+                </div>
+                <div className="col-md-4 col-sm-12 mb-3 d-flex align-items-center">
+                    <button
+                        type="button"
+                        className="btn btn-dark"
+                        onClick={() => setIsModalOpen(true)}
+                    >
+                        <i className="bi bi-plus"></i> Agrega una reservación
+                    </button>
+                </div>
+                {isModalOpen && (
+                    <>
+                        {/* Renderiza el backdrop */}
+                        <div className="modal-backdrop fade show"></div>
+                        <NewBookingModal
+                            isOpen={isModalOpen}
+                            onClose={() => setIsModalOpen(false)}
+                        />
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
 
 export default Bookings;
---
+++ src/services/userService.jsx
@@ -0,0 +1,14 @@
+import { defaultAxios, defaultAxiosWithToken } from "../utils/http";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+const findAllUsers = async () => (await defaultAxiosWithToken.get('/users')).data;
+
+export const useGetUsers = () => {
+    const { data, isLoading, isError, isSuccess, error } = useQuery({
+        queryKey: ['users'],
+        queryFn: () => findAllUsers(),
+        onSuccess:(data) => setBrands(data?.data),
+        refetchOnWindowFocus:false
+    });
+    return { data, isLoading, isError, isSuccess, error };
+}
