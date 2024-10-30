ed20bd30bc9aadf38d835a2baf26d810841ceddc
DiegoMajano <dmajano@gmail.com>
2024-10-30 17:10:01 -0600

    agregando vista de reservaciones: primera version

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../components/auth/Login';
 import Index from '../components/main/Index';
+import Bookings from '../components/bookings/Bookings';
 
 export default function Rutas() {
     const demoWindow = () => window; // Definir la función para la propiedad window
@@ -8,8 +9,10 @@ export default function Rutas() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Index window={demoWindow} />}/>
                 <Route path='/login' element={<Login />} />
                 <Route path='/index' element={<Index window={demoWindow} />} /> {/* Pasa window a Index */}
+                <Route path='/reservations' element={<Bookings />} />
             </Routes>
         </BrowserRouter>
     );
