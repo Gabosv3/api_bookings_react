f0c86318bda35400276c738c1e6f7da500791046
Josías <josiasalvarengaromero@gmail.com>
2024-11-03 09:42:37 -0600

    Update http.js

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const token = sessionStorage.getItem('token_bookings');
 
 export const defaultAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    //baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://apibookingsaccomodations-production.up.railway.app/api/V1",
     headers: {
         "Accept": 'application/json',
         "Content-Type": "application/json"
@@ -11,7 +12,8 @@ export const defaultAxios = axios.create({
 });
 
 export const defaultAxiosWithToken = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    //baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://apibookingsaccomodations-production.up.railway.app/api/V1",
     headers: {
         "Accept": 'application/json',
         "Content-Type": "application/json",
